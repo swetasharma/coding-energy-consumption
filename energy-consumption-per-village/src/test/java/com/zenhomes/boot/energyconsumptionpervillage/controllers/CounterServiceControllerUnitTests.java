3e8a3067be9e7b96324d4a2d40d38522bd6c80df
Sweta Sharma <sweta.sharma@siemens.com>
2020-02-14 17:11:32 +0530

    added unit test for empty consumption report

--- energy-consumption-per-village/src/test/java/com/zenhomes/boot/energyconsumptionpervillage/controllers/CounterServiceControllerUnitTests.java
+++ energy-consumption-per-village/src/test/java/com/zenhomes/boot/energyconsumptionpervillage/controllers/CounterServiceControllerUnitTests.java
@@ -1,17 +1,42 @@
 package com.zenhomes.boot.energyconsumptionpervillage.controllers;
-
 import com.zenhomes.boot.energyconsumptionpervillage.dto.CounterRegister;
+import com.zenhomes.boot.energyconsumptionpervillage.dto.EnergyConsumption;
 import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CounterServiceControllerUnitTests {
 
     @Test
     public void testSuccessCreateCounterCallback(){
-        CounterController counterController =  new CounterController();
+        //here you need to implement actual result and expected result
+        // you need to return some json value which will help you to compare actual result and expected result and use assertEquals to compare expected and actual result
+
+        CounterController counterController = new CounterController();
         CounterRegister counterRegister = new CounterRegister();
         counterRegister.setCounter_id(1);
         counterRegister.setAmount(1000);
         counterController.createCounterCallback(counterRegister);
+    }
+
+    @Test
+    public void testSuccessConsumptionReport(){
+        CounterController counterController = new CounterController();
+        Map<String, List<EnergyConsumption>> actualResult = counterController.consumption_report();
+        //compare actual result and expected result by using assert equals
+    }
+
+    @Test
+    public void testEmptyConsumptionReport(){
+        CounterController counterController = new CounterController();
+        Map<String, List<EnergyConsumption>> actualResult = counterController.consumption_report();
+        List<EnergyConsumption> EnergyConsumptionList = new ArrayList<>();
+        Map<String, List<EnergyConsumption>> expectedResult = new HashMap<>();
+        expectedResult.put("villages", EnergyConsumptionList);
+        assertEquals(expectedResult.get("villages"), actualResult.getOrDefault("villages", actualResult.get("villages")));
+        assertEquals(expectedResult.size(), 0);
     }
 
 

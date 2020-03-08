4982891868a111dd62e4414f4e922c3d09af5cdb
Sweta Sharma <sweta.sharma@siemens.com>
2020-03-08 15:33:04 +0530

    added databaseconfig and fixed JDBC Template configuration error

---
+++ energy-consumption-per-village/src/main/java/com/zenhomes/boot/energyconsumptionpervillage/config/DatabaseConfig.java
@@ -0,0 +1,17 @@
+package com.zenhomes.boot.energyconsumptionpervillage.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan(basePackages = "com.zenhomes.boot.energyconsumptionpervillage")
+@PropertySource(value = "application.properties")
+public class AppConfig {
+
+
+}

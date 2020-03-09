5692fecd725d5cb7692ea8ec3d85cd3eefdc5e10
Sweta Sharma <sweta.sharma@siemens.com>
2020-03-09 13:14:11 +0530

    testes the connection with mysql

--- energy-consumption-per-village/src/main/java/com/zenhomes/boot/energyconsumptionpervillage/controllers/VillageController.java
+++ energy-consumption-per-village/src/main/java/com/zenhomes/boot/energyconsumptionpervillage/controllers/VillageController.java
@@ -1,15 +1,23 @@
 package com.zenhomes.boot.energyconsumptionpervillage.controllers;
 
 import com.zenhomes.boot.energyconsumptionpervillage.models.Village;
+import com.zenhomes.boot.energyconsumptionpervillage.services.VillageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VillageController {
-    @GetMapping(value = "/villages", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Village> getVillages(){
-        return null;
+    @Autowired
+    VillageService villageService;
+
+    @GetMapping(value = "/villages/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Village> getVillageById(@PathVariable Integer id){
+        Village village =  villageService.findById(id);
+        return new ResponseEntity<>(village, HttpStatus.FOUND);
     }
 }

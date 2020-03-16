e94c8ad5236ecd65d1adce5f160433fb4e6ee55e
Sweta Sharma <sweta.sharma@siemens.com>
2020-03-16 16:07:49 +0530

    removed unnecessary classpath

--- energy-consumption-per-village/src/main/java/com/zenhomes/boot/energyconsumptionpervillage/EnergyConsumptionPerVillageApplication.java
+++ energy-consumption-per-village/src/main/java/com/zenhomes/boot/energyconsumptionpervillage/EnergyConsumptionPerVillageApplication.java
@@ -2,10 +2,8 @@ package com.zenhomes.boot.energyconsumptionpervillage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource({"classpath:application.properties"})
 public class EnergyConsumptionPerVillageApplication {
 
 	public static void main(String[] args) {
